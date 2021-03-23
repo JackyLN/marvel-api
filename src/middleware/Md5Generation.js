5f508cb883fd38a10496ac00f3e5b76c59a591dc
Jacky Le <lenghia1991@gmail.com>
2021-03-23 18:08:00 +0800

    add middleware to generate MD5 hash

---
+++ src/middleware/Md5Generation.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+const config = require('../../config');
+
+const createMd5 = () => {
+  const timespan = Date.now();
+
+  const data = timespan + config.marvel.PRIVATE_KEY + config.marvel.PUBLIC_KEY;
+  return crypto.createHash('md5').update(data).digest("hex");
+}
+
+module.exports = createMd5;
